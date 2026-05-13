d65087091c74c2a5ef23a0999ce720d2c45cf88b
Bruxe-Dev <mnibeza23@gmail.com>
2026-05-13 19:17:14 +0200

    added some imports for the firebase configurations

--- apps/api/src/lib/firebase.ts
+++ apps/api/src/lib/firebase.ts
@@ -1,1 +1,3 @@
 import { initializeApp, getApps, cert, type App } from 'firebase-admin/app'
+import { getDatabase, type Database } from 'firebase-admin/database'
+import { getAuth, type Auth } from 'firebase-admin/auth'
