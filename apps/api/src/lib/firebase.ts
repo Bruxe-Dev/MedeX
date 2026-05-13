f340b6c6449515727f60ce31f1c3084c058d3122
Bruxe-Dev <mnibeza23@gmail.com>
2026-05-13 19:51:41 +0200

    implemented a function to create a firebase application

--- apps/api/src/lib/firebase.ts
+++ apps/api/src/lib/firebase.ts
@@ -3,7 +3,18 @@ import { getDatabase, type Database } from 'firebase-admin/database'
 import { getAuth, type Auth } from 'firebase-admin/auth'
 
 function createFirebaseApp(): App {
-    if (getApps.length > 0) {
-        return getApps()[0];
+    if (getApps().length > 0) {
+        return getApps()[0]!;
     }
+
+    return initializeApp({
+        credential: cert({
+            projectId: process.env.FIREBASE_PROJECT_ID!,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+        }),
+        databaseURL: process.env.FIREBASE_DATABASE_URL!,
+    });
 }
+
+const app = createFirebaseApp()
