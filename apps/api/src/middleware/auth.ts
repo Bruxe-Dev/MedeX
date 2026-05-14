d6d2c91e0f6f6332445195d4cc0d315def3bd80c
Bruxe-Dev <mnibeza23@gmail.com>
2026-05-14 15:44:11 +0200

    added an auth toekn decod method

--- apps/api/src/middleware/auth.ts
+++ apps/api/src/middleware/auth.ts
@@ -2,6 +2,8 @@ import type { Context, Next } from "hono";
 import type { DecodedIdToken } from "firebase-admin/auth";
 import { auth } from "../lib/firebase.js";
 import type { UserRole } from "../../../../src/types/database.types.js";
+import { success } from "zod";
+import { error } from "node:console";
 
 export type AuthUser = {
     uid: string;
@@ -22,5 +24,20 @@ export async function authMiddleware(c: Context, next: Next): Promise<Response |
         return c.json({
             success: false, error: "Missing Authorization header"
         }, 401)
+    }
+
+    const parts = authHeader.split(" ")
+    if (parts.length !== 2 || parts[0] !== 'Bearer') {
+        return c.json({
+            success: false, error: "Invalid Token. Expected Bearer <token>"
+        }, 401)
+    }
+
+    const token = parts[1];
+
+    try {
+        const decoded: DecodedIdToken = await auth.verifyIdToken(token);
+    } catch (err) {
+
     }
 }
