4a6018b6ac67acdaa8721120f5d62b6667a9c495
Bruxe-Dev <mnibeza23@gmail.com>
2026-05-14 12:52:06 +0200

    made a authUser type to use in the Auth Controller

--- apps/api/src/middleware/auth.ts
+++ apps/api/src/middleware/auth.ts
@@ -2,3 +2,11 @@ import type { Context, Next } from "hono";
 import type { DecodedIdToken } from "firebase-admin/auth";
 import { auth } from "../lib/firebase.js";
 import type { UserRole } from "../../../../src/types/database.types.js";
+
+export type AutherUser = {
+    uid: string;
+    email: string | undefined;
+    role: UserRole;
+    hospitalId?: string;
+    pharmacyId?: string;
+}
