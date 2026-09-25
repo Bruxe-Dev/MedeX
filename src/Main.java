246a93af3619feda5a33bb8f90a17052afe62e8d
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-25 12:02:21 +0200

    Realized that the Main class was not properly defined so, I refactored some a little.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 import java.io.*;
 
-public static void main(){
-    Medication metformin = new Medication("Metformin","500mg");
+public class Main{
+    public static void main(String[] args) {
+        Medication metformin = new Medication("Metformin", "500mg");
 
-    //String metDosage = metformin.dosage;
-    System.out.println(metformin.getDosage());
-    System.out.println(metformin.getName());
+        // String metDosage = metformin.dosage;
+        System.out.println(metformin.getDosage());
+        System.out.println(metformin.getName());
 
-    Pharmacy ubumwe = new Pharmacy("Ubumwe","Nyarutarama","+250794889741");
+        Pharmacy ubumwe = new Pharmacy("Ubumwe", "Nyarutarama", "+250794889741");
 
-    System.out.println(ubumwe.getName());
-    System.out.println(ubumwe.getLocation());
-    System.out.print(ubumwe.getPhoneNumber());
+        System.out.println(ubumwe.getName());
+        System.out.println(ubumwe.getLocation());
+        System.out.print(ubumwe.getPhoneNumber());
+    }
 }
